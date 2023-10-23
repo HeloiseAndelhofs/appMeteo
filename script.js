8d447e2b5927ae63415a515173b61909dcb01be4
HeloiseAndelhofs <heloise.andelhofs@proximus.be>
2023-10-23 12:03:42 +0200

    forEach works

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const weatherTemplate = document.getElementById("weatherTemplate");
 let weatherOutput = document.getElementById("weatherOutput");
 const apiKey = "03368b30afe0d56755bf6d1ce4341ad4";
 const city = "Bruxelles";
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=fr&cnt=1&units=metric&timezone=Z`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=fr&cnt=8&units=metric&timezone=Z`;
 
 //fetch
 fetch(apiUrl)
@@ -14,35 +14,25 @@ fetch(apiUrl)
 })
 .then((responseJson) => {
     const list = responseJson.list;
-    const temp = list[0].main.temp_min;
-    const caption = list[0].weather[0].description;
-    const date = list[0].dt_txt;
-    const formattedDate = date.toLocaleString('fr-FR', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-    });
     const location = responseJson.city.name;
     const day = new Date();
     const formattedDay = day.toLocaleString('fr-FR', {
         weekday: "long",
-      });;
-
-
-list.forEach(e => {
-    weatherOutput.innerHTML = `
+      });
+    const date = new Date();
+    const hour = date.getHours();
+    list.forEach(e => {
+    weatherOutput.innerHTML += `
+         <h2>${location}</h2>
+         <h2>${date, hour}</h2>
          <li>
-           <div>${e.formattedDay}</div>
-           <div>${formattedDate}</div>
-           <div>${location}</div>
-           <div>${temp} C°</div>
-           <div>${caption}</div>
+           <div>${formattedDay}</div>
+           <div>${e.main.temp} C°</div>
+           <div>${e.weather[0].description}</div>
         </li>`
+    });
 })
-})
+
 .catch((error) => {
     console.log(error);
 });
