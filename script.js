09647328f07d9fa42650383210a7e7bd0895c4e0
HeloiseAndelhofs <heloise.andelhofs@proximus.be>
2023-10-23 14:14:26 +0200

    add icon and city in url

--- script.js
+++ script.js
@@ -4,35 +4,37 @@ const button = document.getElementById("button");
 const weatherTemplate = document.getElementById("weatherTemplate");
 let weatherOutput = document.getElementById("weatherOutput");
 const apiKey = "03368b30afe0d56755bf6d1ce4341ad4";
-const city = "Bruxelles";
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=fr&cnt=8&units=metric&timezone=Z`;
 
-//fetch
-fetch(apiUrl)
-.then((response) => {
-    return response.json()
-})
-.then((responseJson) => {
-    const list = responseJson.list;
-    const location = responseJson.city.name;
-    const day = new Date();
-    const formattedDay = day.toLocaleString('fr-FR', {
-        weekday: "long",
-      });
-    const date = new Date();
-    const hour = date.getHours();
-    list.forEach(e => {
-    weatherOutput.innerHTML += `
-         <h2>${location}</h2>
-         <h2>${date, hour}</h2>
-         <li>
-           <div>${formattedDay}</div>
-           <div>${e.main.temp} C°</div>
-           <div>${e.weather[0].description}</div>
-        </li>`
+button.addEventListener("click", () => {
+    let city = cityName.value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=fr&cnt=8&units=metric&timezone=Z`;
+    
+    //fetch
+    fetch(apiUrl)
+    .then((response) => {
+        return response.json()
+    })
+    .then((responseJson) => {
+        const list = responseJson.list;
+        const location = responseJson.city.name;
+        const day = new Date();
+        const formattedDay = day.toLocaleString('fr-FR', {
+            weekday: "long",
+        });
+        weatherOutput.innerHTML = `<h2>${location}</h2>`;
+        list.forEach(e => {
+            weatherOutput.innerHTML += `
+            <li>
+            <div>${formattedDay}</div>
+            <div>${e.dt_txt}</div>     
+            <img src="https://openweathermap.org/img/wn/${e.weather[0].icon}@2x.png" alt="">
+            <div>${e.main.temp} C°</div>
+            <div>${e.weather[0].description}</div>
+            </li>`
+        });
+    })
+    
+    .catch((error) => {
+        console.log(error);
     });
 })
-
-.catch((error) => {
-    console.log(error);
-});
