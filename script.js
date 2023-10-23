a97ed2f6e92c98f7687e7f65faa6e7df37da7d56
HeloiseAndelhofs <heloise.andelhofs@proximus.be>
2023-10-23 15:11:15 +0200

    add event listener

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const weatherTemplate = document.getElementById("weatherTemplate");
 let weatherOutput = document.getElementById("weatherOutput");
 const apiKey = "03368b30afe0d56755bf6d1ce4341ad4";
 
+//fetch
 button.addEventListener("click", () => {
-    let city = cityName.value;
+    const city = cityName.value;
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=fr&cnt=8&units=metric&timezone=Z`;
-    
-    //fetch
     fetch(apiUrl)
     .then((response) => {
         return response.json()
@@ -19,22 +18,22 @@ button.addEventListener("click", () => {
         const location = responseJson.city.name;
         const day = new Date();
         const formattedDay = day.toLocaleString('fr-FR', {
-            weekday: "long",
-        });
-        weatherOutput.innerHTML = `<h2>${location}</h2>`;
-        list.forEach(e => {
-            weatherOutput.innerHTML += `
-            <li>
-            <div>${formattedDay}</div>
-            <div>${e.dt_txt}</div>     
-            <img src="https://openweathermap.org/img/wn/${e.weather[0].icon}@2x.png" alt="">
-            <div>${e.main.temp} C°</div>
-            <div>${e.weather[0].description}</div>
-            </li>`
-        });
-    })
-    
-    .catch((error) => {
-        console.log(error);
+        weekday: "long",
     });
+    weatherOutput.innerHTML = `<h2>${location}</h2>`;
+    list.forEach(e => {
+        weatherOutput.innerHTML += `
+        <li>
+        <div>${formattedDay}</div>
+        <div>${e.dt_txt}</div>
+        <img src="https://openweathermap.org/img/wn/${e.weather[0].icon}@2x.png" alt="">
+        <div>${e.main.temp} C°</div>
+        <div>${e.weather[0].description}</div>
+        </li>`
+    });
+})
+
+.catch((error) => {
+    console.log(error);
+});
 })
